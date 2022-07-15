b59156c693ad64011875e64d33dc3c041b59753e
Krader <hiimkol@gmail.com>
2022-07-16 00:27:59 +0700

    - create deployment and service k8s - update reverse-proxy endpoint for frontend - add credential files to gitignore - update version tag travis and docker compose

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4e7b9381619546ae9a9030b3f2b431f-284064503.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
